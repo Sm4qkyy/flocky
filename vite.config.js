494319e7cc8cdb3da52d8bfe75263965c83a6e25
Sm4qkyy <164244695+Sm4qkyy@users.noreply.github.com>
2026-05-16 14:07:41 +0300

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/flocky/',
   plugins: [react(), basicSsl()],
   server: {
     host: '0.0.0.0',
